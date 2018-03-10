f6c2236e674ab830fd9f87ae5f004e17388e0ba7
Светлана Плотникова <uzer1874@rambler.ru>
2018-03-10 09:08:48 +0000

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,117 @@
+﻿<?php
+/**
+ * @param $array
+ * @param bool $bool
+ * @return string
+ */
+function int_arr($array){
+    foreach ($array as $value){
+        if(!is_numeric($value)){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+function task1($array, $bool = false){
+    if($bool){
+        $str = '';
+        foreach ($array as $value){
+             $str = $str.' '.$value;
+        }
+        return $str;
+    } else {
+        foreach ($array as $value){
+            echo '<p>'.$value.'</p>';
+        }
+    }
+}
+
+function task2($array,$op){
+    $m = ['+','-','*','/'];
+    if((!is_array($array))||(!in_array($op,$m))||(!int_arr($array))) {
+        echo 'Некорректные параметры ввода';
+    } else {
+        
+		$str = implode($op, $array);
+        eval('$res = ' . $str . ';');
+        echo $res;
+    }
+}
+
+
+/**/
+
+
+function task3(){
+    $array = func_get_args();
+    $op = $array[0];
+    unset($array[0]);
+    $m = ['+','-','*','/'];
+    if((!in_array($op,$m))||(!int_arr($array))){
+        echo 'Некорректные параметры';
+    } else {
+        $str = implode($op, $array);
+        eval('$res = ' . $str . ';');
+        echo $str.' = '.$res;
+    }
+}
+
+
+function task4($num1,$num2){
+    $num1 = abs($num1);
+    $num2 = abs($num2);
+    if(is_numeric($num1)&&is_numeric($num2)&&(!is_float($num1+$num2))){
+        echo '<table border = "1">';
+        for ($n = 1; $n < $num1 + 1; $n++){
+            echo '<tr>';
+            for($i = 1; $i < $num2 +1; $i++){
+                echo '<td>'.$i.'*'.$n.'='.$i*$n.'</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+    }else{
+        echo 'Неверные параметры ввода';
+    }
+}
+
+function task5($str){
+    $str = strtolower($str);
+    $str = str_replace(" ","",$str);
+	$str1 = strrev($str);
+    if($str===$str1){
+	    return true;
+	} else {
+	    return false;
+	}
+		
+}
+function task ($result){
+    if($result === true){
+        echo 'Эта строка палиндром';
+    } else {
+        echo 'Эта строка не палиндром';
+    }
+		
+}
+
+function task6(){
+    $time = getdate();
+    echo $time['mday'].'.'.$time['mon'].'.'.$time['year'].' '.$time['hours'].':'.$time['minutes'].'<br>';
+    $date = '24.02.2016 00:00:00';
+    echo $timestamp = strtotime($date);
+}
+
+function task7($str,$symb1,$symb2){
+    $str = str_replace($symb1,$symb2,$str);
+    return $str;
+}
+function task8($file){
+    readfile($file);
+
+}
+function task9($handle, $str){
+    file_put_contents($handle, $str);
+}
---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<?php
+require('functions.php');
+
+echo task1(['заяц и волк','заяц и медведь','заяц и лиса'],true);
+echo '<br>';
+echo task1(['заяц и волк','заяц и медведь','заяц и лиса']);
+echo task2([1,3,3],'+');
+echo '<br>';
+echo task2([1,2,3.5],'/');
+echo '<br>';
+echo task3('+',1,2,3,5,10.5,1155).'<br>';
+echo task4(5,5.2).'<br>';
+task4(-5,5);
+//$result = task5('Ежу хуже');
+$result = task5('Aba Ba');
+task($result);
+echo '<br><br>';
+task6();
+echo '<br><br>';
+task8('test.txt');
+task9(anothertest.txt, 'Hello again!');
+echo '<br>'.task7('Карл у Клары украл Кораллы','К','');
+echo '<br>'.task7('Две бутылки лимонада','Две','Три');
